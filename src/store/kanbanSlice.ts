1cc6af87c86bba8269a7dd4d214d0db87ff8a013
1ucyhan <lucehanblank@gmail.com>
2025-05-17 11:01:06 -0700

    setting up redux store

---
+++ src/store/kanbanSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+interface Task {
+    id: string
+    name: string
+    columnName: string
+}
+
+interface KanbanState {
+    tasks: Task[]
+}
+
+const initialState: KanbanState = {
+    tasks: []
+}
+
+// from redux documentation: All you have to do is define a name for this slice, 
+// write an object that has some reducer functions in it, and it generates the 
+// corresponding action code automatically.
+export const KanbanSlice = createSlice({
+    name: 'kanban',
+    initialState,
+    reducers: {
+        addTask: (state, action: PayloadAction<{name: string; columnName: string;}>) => {
+            // define a new task...
+            const newTask: Task = {
+                id: crypto.randomUUID(),
+                name: action.payload.name,
+                columnName: action.payload.columnName
+            };
+            // push this new task to the list
+            state.tasks.push(newTask);
+        },
+        deleteTask: (state, action: PayloadAction<{id: string}>) => {
+            // get every single task other than that one task
+            state.tasks = state.tasks.filter(task => task.id != action.payload.id);
+        },
+        moveTask: (state, action: PayloadAction<{id: string; columnName: string;}>) => {
+            // just changing the columnName..?
+            const specificTask = state.tasks.find(task => task.id === action.payload.id);
+            if (specificTask) {
+                specificTask.columnName = action.payload.columnName
+            }
+        }
+    }
+})
+
