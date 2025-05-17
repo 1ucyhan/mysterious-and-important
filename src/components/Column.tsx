877a6766002094b9f17a70ca09e92c76860ce5da
1ucyhan <lucehanblank@gmail.com>
2025-05-16 23:27:02 -0700

    add task btn

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import plus from '/plus.svg'
 
 const Header = styled.h3`
     font-size: 36px;
@@ -11,6 +12,8 @@ const Header = styled.h3`
     width: fit;
 `
 const Background = styled.div`
+    display: flex;
+    flex-direction: column; 
     margin-top: 40px;
     padding: 30px;
     background-color: white;
@@ -20,6 +23,21 @@ const Background = styled.div`
     box-shadow: 20px 20px 0px 0px #000000;
 `
 
+const AddTaskButton = styled.button`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    gap: 30px;
+    margin-top: auto;
+    width: fill;
+    font-family: ppFont;
+    bottom: 30px;
+    padding: 10px 30px;
+    border-radius: 30px;
+    background-color: white;
+    font-size: 18px;
+    border: 1px black solid;
+`
 interface ColumnProps {
     title: string;
 }
@@ -28,7 +46,12 @@ const Column: React.FC<ColumnProps> = ({title}) => {
     return (
         <Background>
             <Header>{title}</Header>
+            <AddTaskButton>
+                <img src="/plus.svg" alt="plus icon" />
+                Yet Another Task
+            </AddTaskButton>
         </Background>
+        
     );
 };
 
